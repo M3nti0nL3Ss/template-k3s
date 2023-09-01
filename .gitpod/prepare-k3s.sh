9f2dc7e5168a97d3dd04624e115ad124d5c6842b
Ayoub Hebbaj <30575106+M3nti0nL3Ss@users.noreply.github.com>
2023-09-01 17:16:52 +0100

    Adding minikube and k9s

--- .gitpod/prepare-k3s.sh
+++ .gitpod/prepare-k3s.sh
@@ -50,6 +50,16 @@ mkdir -p ~/.kube
 echo "✅ k3s server is ready"
 sudo curl -o /usr/bin/kubectl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x /usr/bin/kubectl
+
+sudo curl -o /usr/bin//minikube -LO "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
+sudo chmod +x /usr/bin/minikube
+
+curl -LO https://github.com/derailed/k9s/releases/download/v0.27.4/k9s_Linux_amd64.tar.gz
+tar -xvf k9s_Linux_amd64.tar.gz
+ls -al
 kubectl get pods --all-namespaces
+
+
+
 
 touch "${k3sreadylock}"
